5c82e5d65bb24ec96f4da1cb00b9fa89a9f4e84e
DannnLim <Dannylim-@hotmail.com>
2022-06-06 22:26:17 +1000

    commit tree ca840385a1f574c6543ca3a42191a8b060fe7bd1

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
+const num1 = 2, num2 = 31;
 
-// Write your code here
+let multiply = num1 * num2;
+
+let random = Math.ceil(Math.random() * 12) + 1;
+
+// console.log(random)
+
+let num3 = 20, num4 = 5;
+let mod = num3 / num4;
+
+// console.log(mod)
+
+let max = Math.max(3, 6, 9, 12, 20)
